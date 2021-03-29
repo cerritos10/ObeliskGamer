4ddf506cd5a3723b47de65beb9d0e8c3c5b7f375
cerritos <mu.10cerritos@gmail.com>
2021-03-29 11:31:32 -0600

    añadiendo al carrito

--- components/cart.php
+++ components/cart.php
@@ -1,3 +1,63 @@
+<?php 
+  session_start();
+  include('../php/conexion.php');
+  if (isset($_SESSION['carrito'])) {
+    if (isset($_GET['id'])) {
+      $arreglo = $_SESSION['carrito'];
+      $encontro = false;
+      $numero = 0;
+      for ($i=0; $i < count($arreglo) ; $i++) { 
+        if ($arreglo[$i]['Id'] == $_GET['id']) {
+          $encontro = true;
+          $numero = $i;
+        }
+      }
+      if ($encontro == true) {
+        $arreglo[$numero]['Cantidad'] = $arreglo[$numero]['Cantidad']+1;
+        $_SESSION['carrito'] = $arreglo;
+      }
+      else {
+        $nombre = "";      
+        $precio = "";      
+        $imagen = "";
+        $res = $conexion -> query('select * from productos where id_producto =' .$_GET['id']) or die($conexion->error);       
+        $fila = mysqli_fetch_row($res);
+        $nombre = $fila[1];  
+        $precio = $fila[3];     
+        $imagen = $fila[4];
+        $arregloNuevo = array(
+          'Id' => $_GET['id'],
+          'Nombre' => $nombre,
+          'Precio' => $precio,
+          'Imagen' => $imagen,
+          'Cantidad' => 1          
+        );
+        array_push($arreglo, $arregloNuevo);
+        $_SESSION['carrito'] = $arreglo;
+      }
+    }
+  }
+  else {
+    if (isset($_GET['id'])) {
+      $nombre = "";      
+      $precio = "";      
+      $imagen = "";
+      $res = $conexion -> query('select * from productos where id_producto =' .$_GET['id']) or die($conexion->error);       
+      $fila = mysqli_fetch_row($res);
+      $nombre = $fila[1];  
+      $precio = $fila[3];     
+      $imagen = $fila[4];
+      $arreglo[] = array(
+        'Id' => $_GET['id'],
+        'Nombre' => $nombre,
+        'Precio' => $precio,
+        'Imagen' => $imagen,
+        'Cantidad' => 1          
+      );
+      $_SESSION['carrito'] = $arreglo;
+    }
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -42,52 +102,36 @@
                   </tr>
                 </thead>
                 <tbody>
+                <?php 
+                  if (isset($_SESSION['carrito'])) {
+                    $arregloCarrito = $_SESSION['carrito'];
+                    for ($i = 0; $i < count($arregloCarrito); $i++) { 
+                ?>
                   <tr>
                     <td class="product-thumbnail">
-                      <img src="../assets/images/razer-v-m.jpg" alt="Image" class="img-fluid">
+                      <img src="../assets/images/<?php echo $arregloCarrito[$i]['Imagen']; ?>" alt="Image" class="img-fluid">
                     </td>
                     <td class="product-name">
-                      <h2 class="h5 text-black">Rizer Viper</h2>
+                      <h2 class="h5 text-black"><?php echo $arregloCarrito[$i]['Nombre']; ?></h2>
                     </td>
-                    <td>$49.00</td>
+                    <td>$<?php echo $arregloCarrito[$i]['Precio']; ?></td>
                     <td>
                       <div class="input-group mb-1 centrado" style="max-width: 120px;">
                         <div class="input-group-prepend">
                           <button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>
                         </div>
-                        <input type="text" class="form-control text-center cantidad" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                        <input type="text" class="form-control text-center cantidad" value="<?php echo $arregloCarrito[$i]['Cantidad']; ?>" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
                         <div class="input-group-append">
                           <button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>
                         </div>
                       </div>
-
                     </td>
-                    <td>$49.00</td>
+                    <td>$<?php echo $arregloCarrito[$i]['Precio']*$arregloCarrito[$i]['Cantidad'];?></td>
                     <td><a href="#" class="btn-gradi">X</a></td>
                   </tr>
 
-                  <tr>
-                    <td class="product-thumbnail">
-                      <img src="../assets/images/razer-nari.jpg" alt="Image" class="img-fluid">
-                    </td>
-                    <td class="product-name">
-                      <h2 class="h5 text-black">Polo Shirt</h2>
-                    </td>
-                    <td>$49.00</td>
-                    <td>
-                      <div class="input-group mb-1 centrado" style="max-width: 120px;">
-                      <div class="input-group-prepend">
-                          <button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>
-                        </div>
-                        <input type="text" class="form-control text-center cantidad" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
-                        <div class="input-group-append">
-                          <button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>
-                        </div>
-                      </div> 
-                    </td>
-                    <td>$49.00</td>
-                    <td><a href="#" class="btn-gradi">X</a></td>
-                  </tr>
+                <?php }} ?>
+
                 </tbody>
               </table>
             </div>
